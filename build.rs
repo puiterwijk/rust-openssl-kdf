d75835578cdea15d1b69ddd3b65d0c4e84a8ccb6
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-11-18 17:09:16 +0100

    Make implementation an enum
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- build.rs
+++ build.rs
@@ -1,6 +1,25 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[allow(unused)]
+enum Implementation {
+    Ossl11,
+    Ossl3,
+    Custom,
+}
+
+impl ToString for Implementation {
+    fn to_string(&self) -> String {
+        match self {
+            Implementation::Ossl11 => "ossl11",
+            Implementation::Ossl3 => "ossl3",
+            Implementation::Custom => "custom",
+        }
+        .to_string()
+    }
+}
+
 #[allow(unreachable_code)]
 fn main() {
-    let implementation: &str;
+    let implementation: Implementation;
 
     #[cfg(not(feature = "force_custom"))]
     {
@@ -10,7 +29,7 @@ fn main() {
             // Determine if this version of OpenSSL has the requisite patch backported
             let kdf_h_cts = std::fs::read_to_string("/usr/include/openssl/kdf.h").unwrap();
             if kdf_h_cts.contains("KDF_CTX_new_id") {
-                implementation = "ossl11";
+                implementation = Implementation::Ossl11;
             } else {
                 #[cfg(not(feature = "allow_custom"))]
                 panic!(
@@ -19,10 +38,10 @@ fn main() {
                     use the `allow_custom` feature to allow the use of the custom implementation."
                 );
 
-                implementation = "custom";
+                implementation = Implementation::Custom;
             }
         } else if openssl_version.starts_with("3.") {
-            implementation = "ossl3";
+            implementation = Implementation::Ossl3;
         } else {
             panic!("No usable OpenSSL version detected in {}. You can enable the 'allow_custom' feature.", openssl_version);
         }
@@ -32,14 +51,17 @@ fn main() {
         #[cfg(not(feature = "allow_custom"))]
         panic!("Forcing custom without allowing custom");
 
-        implementation = "custom";
+        implementation = Implementation::Custom;
     }
 
     #[cfg(feature = "deny_custom")]
-    if implementation == "custom" {
+    if implementation == Implementation::Custom {
         panic!("The 'custom' implementation is not allowed");
     }
 
-    println!("cargo:rustc-cfg=implementation=\"{}\"", implementation);
+    println!(
+        "cargo:rustc-cfg=implementation=\"{}\"",
+        implementation.to_string()
+    );
     println!("cargo::rustc-link-lib=crypto");
 }
